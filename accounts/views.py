88eb5759d315e0eb238185db364c0051680b5c94
Maripia Kuti <pionste@gmail.com>
2020-05-28 22:56:01 +0000

    Changed name of login app to accounts and moved register template

--- accounts/views.py
+++ accounts/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 
 
 # Create your views here.
+def login(request):
+    return render(request, 'login.html')
+
+
 def register(request):
     return render(request, 'register.html')
