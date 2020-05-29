79442763e246e79491b72a0b19471983c321cdfa
Maripia Kuti <pionste@gmail.com>
2020-05-29 01:24:00 +0000

    Deleted accounts app

--- accounts/urls.py
+++
@@ -1,8 +0,0 @@
-# from django.urls import path
-# from .views import login, register
-
-
-# urlpatterns = [
-#     path('login', login, name='login'),
-#     path('register', register, name='register')
-# ]
