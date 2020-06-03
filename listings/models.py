2a8087f01e9b2b567437e8e1cdf05d5d9a36d39d
Maripia Kuti <pionste@gmail.com>
2020-06-03 21:40:30 +0000

    Added basic listing model

--- listings/models.py
+++ listings/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 
-class Product(models.Model):
+class listing(models.Model):
     name = models.CharField(max_length=254, default='')
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
