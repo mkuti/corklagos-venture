e86ddd4f4708bb652c3ba469a4d1939360964ccc
Maripia Kuti <pionste@gmail.com>
2020-06-06 17:16:16 +0000

    Added form to match model and to be rendered to edit profile

---
+++ dashboard/forms.py
@@ -0,0 +1,29 @@
+from django import forms
+from .models import Profile
+
+
+class EditProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        # include all fields except for user since this should never change
+        exclude = ('user',)
+
+    def __init__(self, *args, **kwargs):
+        """
+        Adding placeholders and classes
+        """
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'default_business_name': 'Business Name',
+            'default_phone': 'Phone Number',
+            'default_eircode': 'Eircode',
+            'default_city': 'City',
+            'default_street_address': 'Street Address',
+            'default_street_address2': 'Street Address 2',
+            'default_county': 'County, State or Locality',
+        }
+
+        self.fields['default_phone_number'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            self.fields[field].widget.attrs['class'] = 'form-control'
+            self.fields[field].label = False
