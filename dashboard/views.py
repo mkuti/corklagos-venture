8732db56e3a0b55b9a4ed6850bfaf08ecd99686b
Maripia Kuti <pionste@gmail.com>
2020-06-06 02:18:17 +0000

    Creating instance of user and rendering template

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,6 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from .models import Profile
 
 
 # Create your views here.
-def dashboard(request):
-    return render(request, 'dashboard.html')
+@login_required
+def profile(request):
+    """ Display the member's profile after login. """
+    # profile = get_object_or_404(Profile, user=request.user)
+
+    # template = 'profile.html'
+    context = {
+        # 'profile': profile,
+    }
+
+    return render(request, 'profile.html', context)
