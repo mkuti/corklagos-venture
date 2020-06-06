87d1cec2fbcd72fa4fc63e184327f5e85a412b26
Maripia Kuti <pionste@gmail.com>
2020-06-06 02:19:15 +0000

    Imported django user to product model

--- listings/admin.py
+++ listings/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import listing
 # Register your models here.
+
+admin.site.register(listing)
--- listings/models.py
+++ listings/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 
 class listing(models.Model):
     name = models.CharField(max_length=254, default='')
