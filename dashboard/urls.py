de19cf6241c8caa3305f4239dc28fded054dafda
Maripia Kuti <pionste@gmail.com>
2020-06-06 02:17:17 +0000

    Renamed url and view

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import dashboard
+from .views import profile
 
 urlpatterns = [
-    path('', dashboard, name='dashboard'),
+    path('', profile, name='profile'),
 ]
