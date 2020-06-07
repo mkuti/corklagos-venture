73cbf3f5c9375f3d51e3a15b07635dc7a4c4863d
Maripia Kuti <pionste@gmail.com>
2020-06-07 21:04:02 +0000

    Reverted to basic signal for creating profile instance

--- dashboard/models.py
+++ dashboard/models.py
@@ -30,5 +30,6 @@ def create_or_update_user_profile(sender, instance, created, **kwargs):
 
     if created:
         Profile.objects.create(user=instance)
+
     # Existing users: just save the profile
     instance.profile.save()
