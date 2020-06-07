ab055882f28053bedd23cf401e947629540dfaa2
Maripia Kuti <pionste@gmail.com>
2020-06-07 00:04:09 +0000

    Imported and rendered profile form for template

--- dashboard/views.py
+++ dashboard/views.py
@@ -2,17 +2,18 @@ from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .models import Profile
+from .forms import EditProfileForm
 
 
 # Create your views here.
 @login_required
 def profile(request):
     """ Display the member's profile after login. """
-    # profile = get_object_or_404(Profile, user=request.user)
+    profile = get_object_or_404(Profile, user=request.user)
 
-    # template = 'profile.html'
+    form = EditProfileForm(instance=profile)
     context = {
-        # 'profile': profile,
+        'form': form,
     }
 
     return render(request, 'profile.html', context)
