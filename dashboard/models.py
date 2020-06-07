8f2a163c7c6045101800aec0c5fab68923747633
Maripia Kuti <pionste@gmail.com>
2020-06-07 14:53:15 +0000

    Added signal again to create a profile when user get saved

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class Profile(models.Model):
@@ -17,3 +19,16 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.user.username
+
+
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    '''
+    Connect create_user_profile & save_user_profile
+    methods to User model, whenever a save event occurs
+    '''
+
+    if created:
+        Profile.objects.create(user=instance)
+    # Existing users: just save the profile
+    instance.profile.save()
