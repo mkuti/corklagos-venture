f69f0f1103dee3b14c92a9dcc8b1ba871da48490
Maripia Kuti <pionste@gmail.com>
2020-06-07 13:42:52 +0000

    Removed signal from the model as already taken care in adapter;

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.db.models.signals import post_save
-from django.dispatch import receiver
 
 
 class Profile(models.Model):
@@ -19,16 +17,3 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.user.username
-
-
-@receiver(post_save, sender=User)
-def create_or_update_user_profile(sender, instance, created, **kwargs):
-    '''
-    Connect create_user_profile & save_user_profile
-    methods to User model, whenever a save event occurs
-    '''
-
-    if created:
-        Profile.objects.create(user=instance)
-    # Existing users: just save the profile
-    instance.profile.save()
