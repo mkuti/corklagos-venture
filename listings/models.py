c015d61a5a50c665e90287465069bf00ed079f35
Maripia Kuti <pionste@gmail.com>
2020-06-08 23:26:59 +0000

    Updated models of listings app

--- listings/models.py
+++ listings/models.py
@@ -1,11 +1,21 @@
 from django.db import models
-from django.contrib.auth.models import User
 
-class listing(models.Model):
-    name = models.CharField(max_length=254, default='')
+
+class Listings(models.Model):
+    categories = [
+        ('', 'See all'),
+        ('engines', 'Engines'),
+        ('gearboxes', 'Gear Boxes'),
+        ('lamps', 'Lamps'),
+        ('Radiators', 'radiators'),
+        ('bumpers', 'Bumpers'),
+    ]
+
+    name = models.CharField(max_length=50)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image = models.ImageField(upload_to='images')
+    category = models.CharField(max_length=15, choices=categories)
 
     def __str__(self):
         return self.name
