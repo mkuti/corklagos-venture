15da108cc4d7bb545afe34ab1584ce53b231bbd7
Maripia Kuti <pionste@gmail.com>
2020-06-08 23:58:23 +0000

    Added brands to the Listing model and changed name

--- listings/models.py
+++ listings/models.py
@@ -11,14 +11,22 @@ class Listings(models.Model):
         ('bumpers', 'Bumpers'),
     ]
 
-    name = models.CharField(max_length=50)
-    description = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)
-    image = models.ImageField(upload_to='images')
-    category = models.CharField(max_length=15, choices=categories)
+    brands = [
+        ('', 'See all'),
+        ('toyota', 'Toyota'),
+        ('nissan', 'Nissan'),
+        ('honda', 'Honda'),
+    ]
+
+    listing_name = models.CharField(max_length=50)
+    listing_description = models.TextField()
+    listing_price = models.DecimalField(max_digits=6, decimal_places=2)
+    listing_image = models.ImageField(upload_to='images')
+    listing_category = models.CharField(max_length=15, choices=categories, default='')
+    listing_brand = models.CharField(max_length=10, choices=brands, default='')
 
     class Meta:
-        ordering = ['name']
+        ordering = ['listing_name']
 
     def __str__(self):
         return self.name
