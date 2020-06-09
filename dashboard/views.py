a04e84c32f185b66dc9ed69814466e41e17e625a
Maripia Kuti <pionste@gmail.com>
2020-06-09 01:32:06 +0000

    Corrected slightly context

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,19 +1,26 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from .models import Profile
-from .forms import EditProfileForm
+from listings.models import Listings
+from listings.forms import AddListingForm
 
 
 # Create your views here.
 @login_required
-def profile(request):
-    """ Display the member's profile after login. """
-    profile = get_object_or_404(Profile, user=request.user)
+def create_or_edit_listing(request, pk=None):
+    """ Display the member's profile after login.
+    Display current listings
+    Render form to add a new listing """
+    listing = get_object_or_404(Listings, pk=pk) if pk else None
 
-    form = EditProfileForm(instance=profile)
+    if request.method == 'POST':
+        form = AddListingForm(request.POST, request.FILES, instance=listing)
+        if form.is_valid():
+            listing = form.save()
+    else:
+        form = AddListingForm(instance=listing)
+
     context = {
-        'form': form,
+        'form': form
     }
-
     return render(request, 'profile.html', context)
