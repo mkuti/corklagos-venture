aa6a2de925b0dd4935f327cc967d8d094303f7a7
Maripia Kuti <pionste@gmail.com>
2020-06-09 00:34:01 +0000

    Corrected typo in model title

--- listings/admin.py
+++ listings/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import listing
+from .models import Listings
 # Register your models here.
 
-admin.site.register(listing)
+admin.site.register(Listings)
