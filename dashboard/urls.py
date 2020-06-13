8797b449a19cbe3cf1387561e893ce135929cd0c
Maripia Kuti <pionste@gmail.com>
2020-06-13 12:34:29 +0000

    Added url to wire up new view to edit listing details

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import get_and_create_listing, add_profile_details
+from .views import get_and_create_listing, add_profile_details, view_and_edit_listing
 
 urlpatterns = [
     path('', add_profile_details, name='editprofile'),
     path('addlisting/', get_and_create_listing, name='addlisting'),
+    path('editlisting/<int:listing_id>', view_and_edit_listing, name='editlisting'),
 ]
