170c3ec54240f1bfc6eea521be17fcf2662b94d9
Maripia Kuti <pionste@gmail.com>
2020-06-14 23:45:37 +0000

    Added filter field for brand

--- listings/filters.py
+++ listings/filters.py
@@ -1,5 +1,5 @@
 import django_filters
-from .models import Listing, Category
+from .models import Listing, Category, Brand
 from django import forms
 
 
@@ -8,7 +8,11 @@ class ListingFilter(django_filters.FilterSet):
         queryset=Category.objects.all(),
         widget=forms.RadioSelect,
         label=None)
+    listing_brand = django_filters.ModelChoiceFilter(
+        queryset=Brand.objects.all(),
+        widget=forms.RadioSelect,
+        label=None)
 
     class Meta:
         model = Listing
-        fields = ['listing_category']
+        fields = ['listing_category', 'listing_brand']
