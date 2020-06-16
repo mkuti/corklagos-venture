c3d529f65a27caebabace598b5a2fd0d96395a33
Maripia Kuti <pionste@gmail.com>
2020-06-16 23:28:42 +0000

    Wired up add a listing to the view

--- bag/urls.py
+++ bag/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import view_bag
+from .views import view_bag, add_to_bag
 
 urlpatterns = [
     path('', view_bag, name='view_bag'),
+    path('add/<int:listing_id>', add_to_bag, name='add_to_bag')
 ]
