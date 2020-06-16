363fd059cc18c3f0b680350f59450928781821bf
Maripia Kuti <pionste@gmail.com>
2020-06-16 23:10:48 +0000

    Added process to add an item to the cart and store it in session

--- bag/views.py
+++ bag/views.py
@@ -1,6 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 
 
 # Create your views here.
 def view_bag(request):
     return render(request, 'bag.html')
+
+
+def add_to_bag(request, listing_id):
+    listing_quantity = 1
+    bag = request.session.get('bag', {})
+
+    bag[listing_id] = bag.get(listing_id, listing_quantity)
+
+    request.session['bag'] = bag
+
+    return redirect(reverse('listings'))
