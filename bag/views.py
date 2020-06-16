5fc6b4474afd6f055b8921401a07de98a01928a1
Maripia Kuti <pionste@gmail.com>
2020-06-16 23:18:42 +0000

    Add comment and if statement to redirect user to login if not yet

--- bag/views.py
+++ bag/views.py
@@ -1,17 +1,27 @@
 from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
 
 
-# Create your views here.
 def view_bag(request):
     return render(request, 'bag.html')
 
 
 def add_to_bag(request, listing_id):
+    '''
+    If user is not logged in, redirect to login page.
+    Setting quantity of item to 1 as one single item available.
+    Check if bag exists, if not create it.
+    Add listing ID with its fixed quantity
+    Store bag in current user session
+    Redirect to listings when listing added
+    '''
     listing_quantity = 1
     bag = request.session.get('bag', {})
 
-    bag[listing_id] = bag.get(listing_id, listing_quantity)
-
-    request.session['bag'] = bag
+    if request.user.is_authenticated:
+        bag[listing_id] = listing_quantity
+        request.session['bag'] = bag
+    messages.error(request, 'Please log in to buy a listing')
+    return redirect(reverse('login'))
 
     return redirect(reverse('listings'))
