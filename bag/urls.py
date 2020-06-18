55d584104837a8b99784c9065d3909789e467fa0
Maripia Kuti <pionste@gmail.com>
2020-06-18 21:20:19 +0000

    Changed url file of bag app so item can be removed from bag

--- bag/urls.py
+++ bag/urls.py
@@ -4,5 +4,5 @@ from .views import view_bag, add_to_bag, remove_from_bag
 urlpatterns = [
     path('', view_bag, name='view_bag'),
     path('add/<int:listing_id>', add_to_bag, name='add_to_bag'),
-    path('remove/<int:listing_id>', remove_from_bag, name='remove_from_bag')
+    path('remove/<listing_id>', remove_from_bag, name='remove_from_bag')
 ]
