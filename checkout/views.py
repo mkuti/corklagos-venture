246ee78f48248eb92371609f0b79d0bb91aec845
Maripia Kuti <pionste@gmail.com>
2020-06-18 23:19:26 +0000

    Imported profile form and model and render form to checkout.html

--- checkout/views.py
+++ checkout/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from dashboard.forms import EditProfileForm
+from dashboard.models import Profile
 
 
 def checkout_details(request):
-    return render(request, 'checkout.html')
+    profile_form = EditProfileForm()
+
+    context = {
+        'profile_form': profile_form,
+    }
+
+    return render(request, 'checkout.html', context)
 
