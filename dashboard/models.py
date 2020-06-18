efd44487d250e5fc6ea8afe6eac8d119ba34f36f
Maripia Kuti <pionste@gmail.com>
2020-06-18 22:58:34 +0000

    Changed from eircode to postcode

--- dashboard/models.py
+++ dashboard/models.py
@@ -23,7 +23,7 @@ class Profile(models.Model):
     city = models.CharField(max_length=30, blank=False)
     county = models.CharField(max_length=30, blank=True)
     country = models.CharField(max_length=30, blank=True)
-    eircode = models.CharField(max_length=20, blank=True)
+    postcode = models.CharField(max_length=20, blank=True)
 
     def __str__(self):
         return f'{self.user.username} Profile'
