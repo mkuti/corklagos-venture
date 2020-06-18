54389c1fbc69f71d6a52f0daef410c947271cf6c
Maripia Kuti <pionste@gmail.com>
2020-06-18 07:32:14 +0000

    Created url for removing from bag

--- bag/urls.py
+++ bag/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import view_bag, add_to_bag
+from .views import view_bag, add_to_bag, remove_from_bag
 
 urlpatterns = [
     path('', view_bag, name='view_bag'),
-    path('add/<int:listing_id>', add_to_bag, name='add_to_bag')
+    path('add/<int:listing_id>', add_to_bag, name='add_to_bag'),
+    path('remove/<int:listing_id>', remove_from_bag, name='remove_from_bag')
 ]
