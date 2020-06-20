162149a3e13ad5fdea203b4dddfec5c18e96a39a
Maripia Kuti <pionste@gmail.com>
2020-06-20 10:22:57 +0000

    Removed space

--- checkout/views.py
+++ checkout/views.py
@@ -8,6 +8,7 @@ def checkout_details(request):
     '''
     If user has profile, get profile instance
     and display details on profile form.
+    If not, profile_form is empty.
     '''
     try:
         profile = Profile.objects.get(user=request.user)
@@ -20,4 +21,3 @@ def checkout_details(request):
     }
 
     return render(request, 'checkout.html', context)
-
