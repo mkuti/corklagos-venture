77d5321760ea1f25dea2895fdc1bb326f1e266a8
Maripia Kuti <pionste@gmail.com>
2020-06-20 13:20:49 +0000

    Created model for orderlineitem

--- checkout/models.py
+++ checkout/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from dashboard.models import Profile
+from listings.models import Listing
 
-# Create your models here.
+
+class OrderLineItem(models.Model):
+    '''
+    Creates a new table/model
+    including the billing details information from Profile model
+    including the listing information from model imported from listings.models
+    including the quantity
+    including the date
+    '''
+    billing = models.ForeignKey(Profile, null=False, on_delete=models.CASCADE)
+    listing = models.ForeignKey(Listing, null=False, on_delete=models.CASCADE)
+    quantity = models.IntegerField(blank=False)
+    date = models.DateField()
+
+    def __str__(self):
+        '''
+        Returns the quantity of products being bought,
+        the listing name and listing price
+        '''
+        return "{0} {1} @ {2}".format(
+            self.quantity,
+            self.listing.name,
+            self.listing.price)
