10b5680f8ac94c00c2345881a1988fb0a20366a1
Maripia Kuti <pionste@gmail.com>
2020-06-21 00:51:18 +0000

    Corrected typo for the ModelForm name and streetaddress2 field

--- checkout/forms.py
+++ checkout/forms.py
@@ -28,29 +28,28 @@ class MakePaymentForm(forms.Form):
     stripe_id = forms.CharField(widget=forms.HiddenInput)
 
 
-class OrderForm(forms.Form):
+class OrderForm(forms.ModelForm):
     class Meta:
         model = Order
         # include all fields except for date since added automatically
-        exclude = ('date')
+        exclude = ['date']
 
     def __init__(self, *args, **kwargs):
         """
-        Adding placeholders and classes
+        Adding labels and classes
         """
         super().__init__(*args, **kwargs)  # call default init method to set form up
-        placeholders = {
+        labels = {
             'full_name': 'Please enter the name on the card',
-            'street_address': 'Street Address',
-            'street_address2': 'Street Address 2',
+            'street_address1': 'Street Address',
+            'street_address2': 'Street Address (optional)',
             'postcode': 'Postcode',
             'city': 'City',
-            'country': 'Country'
+            'country': 'Country',
         }
 
-        self.fields['full_name'].widget.attrs['autofocus'] = True  # to force cursor to start in business name field
         for field in self.fields:
-            placeholder = placeholders[field]
+            label = labels[field]
             self.fields[field].widget.attrs['placeholder'] = False
-            self.fields[field].label = placeholder
+            self.fields[field].label = label
             self.fields[field].widget.attrs['class'] = 'form-control'
