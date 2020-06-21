1a938603e152d6d53ab0a0e1b6f45d96e8919fb6
Maripia Kuti <pionste@gmail.com>
2020-06-21 20:46:35 +0000

    Corrected typo in id_expiry_year

--- static/js/stripe.js
+++ static/js/stripe.js
@@ -4,12 +4,11 @@ $(function() {
         var card = {
             number: $("#id_credit_card_number").val(),
             expMonth: $("#id_expiry_month").val(),
-            expYear: $("#id_expiry-year").val(),
+            expYear: $("#id_expiry_year").val(),
             cvc: $("#id_cvv").val()
         };
     Stripe.createToken(card, function(status, response) {
         if (status === 200) {
-            console.log(response)
             $("#credit-card-errors").hide();
             $("#id_stripe_id").val(response.id);
 
@@ -22,7 +21,6 @@ $(function() {
             form.submit();
         
         } else {
-            console.log(response)
             $("#stripe-error-message").text(response.error.message);
             $("#credit-card-errors").show();
             $("#validate_card_btn").attr("disabled", false);
