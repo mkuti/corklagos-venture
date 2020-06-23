c5034c6514e05a8ca1fe3d87286683c3be11670d
Maripia Kuti <pionste@gmail.com>
2020-06-23 23:47:12 +0000

    Wired up new view and altered profile url

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,9 +1,29 @@
 from django.urls import path
-from .views import get_and_create_listing, add_profile_details, view_and_edit_listing, delete_listing
+from .views import (
+    dashboard,
+    add_profile_details,
+    get_and_create_listing,
+    view_and_edit_listing,
+    delete_listing
+)
 
 urlpatterns = [
-    path('', add_profile_details, name='editprofile'),
-    path('addlisting/', get_and_create_listing, name='addlisting'),
-    path('editlisting/<int:listing_id>', view_and_edit_listing, name='editlisting'),
-    path('deletelisting/<int:listing_id>', delete_listing, name='deletelisting'),
+    path(
+        '',
+        dashboard,
+        name='dashboard'),
+    path(
+        'profile/',
+        add_profile_details,
+        name='editprofile'),
+    path(
+        'addlisting/',
+        get_and_create_listing,
+        name='addlisting'),
+    path(
+        'editlisting/<int:listing_id>',
+        view_and_edit_listing, name='editlisting'),
+    path(
+        'deletelisting/<int:listing_id>',
+        delete_listing, name='deletelisting'),
 ]
