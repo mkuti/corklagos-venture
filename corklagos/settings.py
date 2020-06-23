fe0e9fdc74b755e6db5eacf09115f14e621f4532
Maripia Kuti <pionste@gmail.com>
2020-06-23 21:53:17 +0000

    Added if statement to send real emails to user when in production

--- corklagos/settings.py
+++ corklagos/settings.py
@@ -127,8 +127,18 @@ are used by the social account app to create the proper callback URLs
 when connecting via social media accounts.'''
 SITE_ID = 1
 
-# temporarily log confirmation emails on console sent by allauth to any new accounts
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+# Django-email
+if 'DEVELOPMENT' in os.environ:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    DEFAULT_FROM_EMAIL = 'info@corklagos.com'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    DEFAULT_FROM_EMAIL = 'info@corklagos.com'
 
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
 ACCOUNT_EMAIL_REQUIRED = True
@@ -137,6 +147,7 @@ ACCOUNT_USERNAME_MIN_LENGTH = 4
 ACCOUNT_PRESERVE_USERNAME_CASING = False
 LOGIN_URL = '/accounts/login/'
 LOGIN_REDIRECT_URL = '/'
+ACCOUNT_EMAIL_CONFIRMATION_AUTHENTICATED_REDIRECT_URL = '/dashboard/'
 ACCOUNT_LOGOUT_ON_GET = False
 ACCOUNT_LOGIN_ON_EMAIL_CONFIRMATION = True
 
