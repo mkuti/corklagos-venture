a1fe3d0d9b8f590ddcc25163edfc9be3af919f87
Maripia Kuti <pionste@gmail.com>
2020-06-24 22:46:15 +0000

    Collected static to staticfiles

--- corklagos/settings.py
+++ corklagos/settings.py
@@ -191,13 +191,16 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 if 'DEVELOPMENT' in os.environ:
     STATIC_URL = '/static/'
-    STATICFILES_DIRS = (
-        os.path.join(BASE_DIR, 'static'),
-    )
+else:
+    STATIC_URL = '/staticfiles/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
