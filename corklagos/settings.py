281b3e7090abbd42f311967032fcd622a813ca01
Maripia Kuti <pionste@gmail.com>
2020-06-24 22:08:23 +0000

    Collected static after making changes in css

--- corklagos/settings.py
+++ corklagos/settings.py
@@ -197,6 +197,8 @@ if 'DEVELOPMENT' in os.environ:
         os.path.join(BASE_DIR, 'static'),
     )
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
