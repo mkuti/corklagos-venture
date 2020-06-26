220d706d1dc1a35339a54b1b2cd1f39d56bedb98
Maripia Kuti <pionste@gmail.com>
2020-06-26 06:09:07 +0000

    Added login required for viewing the shopping bag

--- bag/views.py
+++ bag/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 
+@login_required
 def view_bag(request):
     return render(request, 'bag.html')
 
