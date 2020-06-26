2a93c6f52e41b6e59a91e18b18372d3592921e92
Maripia Kuti <pionste@gmail.com>
2020-06-26 20:10:36 +0000

    Added an if statement to redirect any logged in user to listings

--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 
 
 # Create your views here.
 def home(request):
+    user = request.user
+    if user.is_authenticated:
+        return redirect(reverse('listings'))
+
     return render(request, 'home.html')
 
 
