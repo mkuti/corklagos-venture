08b5aa4b061301e629fbdfa8a3bfd077985e170d
Maripia Kuti <pionste@gmail.com>
2020-06-29 00:59:11 +0000

    Corrected typo

--- listings/views.py
+++ listings/views.py
@@ -7,7 +7,7 @@ def all_listings(request):
     """
     A view to show all listings and include filtered by category
     """
-    listings = Listing.objects.filter(is_active=True)
+    listings = Listing.objects.all()
     filtered_listings = ListingFilter(request.GET, queryset=listings)
 
     return render(request, "listings.html", {'filter': filtered_listings})
