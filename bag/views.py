da829730e2007da68d6c573e8ab9869908b985bf
Maripia Kuti <pionste@gmail.com>
2020-06-29 21:36:47 +0000

    Added try/except statement to redirect dismantler user to listings

--- bag/views.py
+++ bag/views.py
@@ -1,16 +1,37 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from dashboard.models import Profile
 
 
 @login_required
 def view_bag(request):
-    return render(request, 'bag.html')
+    '''
+    If user is a dismantler user type, redirect to listings page.
+    Otherwise render bag.html to view shopping bag
+    where general context will be displayed
+    '''
+
+    try:
+        profile = Profile.objects.get(user=request.user)
+        user_type = profile.user_type
+
+        if user_type == 'dismantler':
+            messages.error(
+                request,
+                'You do not have the correct profile to shop')
+            return redirect(reverse('listings'))
+
+        return render(request, 'bag.html')
+
+    except Profile.DoesNotExist:
+        return render(request, 'bag.html')
 
 
 def add_to_bag(request, listing_id):
     '''
     If user is not logged in, redirect to login page.
+    If user is a dismantler user type, throw an error message.
     Setting quantity of item to 1 as one single item available.
     Getting bag if already exists or initializing it to empty dictionary
     Add listing ID with its fixed quantity
@@ -22,10 +43,29 @@ def add_to_bag(request, listing_id):
     bag = request.session.get('bag', {})
 
     if request.user.is_authenticated:
-        bag[listing_id] = listing_quantity
-        request.session['bag'] = bag
-        messages.success(request, 'We have added the listing to your bag')
-        return redirect(redirect_url)
+        try:
+            profile = Profile.objects.get(user=request.user)
+            user_type = profile.user_type
+
+            if user_type == 'dismantler':
+                messages.error(
+                    request,
+                    'You do not have the correct profile to shop')
+                return redirect(redirect_url)
+            else:
+                bag[listing_id] = listing_quantity
+                request.session['bag'] = bag
+                messages.success(
+                    request,
+                    'We have added the listing to your bag')
+                return redirect(redirect_url)
+
+        except Profile.DoesNotExist:
+            messages.warning(
+                request,
+                'Please add your profile details before shopping.')
+            return redirect(reverse('editprofile'))
+
     messages.error(request, 'Please log in to buy a listing')
     return redirect(reverse('account_login'))
 
