002dc3380aa9d6707f5c235889b3da228abe0713
Maripia Kuti <pionste@gmail.com>
2020-06-29 12:59:22 +0000

    Re-added is_active filter to show listings

--- listings/views.py
+++ listings/views.py
@@ -7,7 +7,7 @@ def all_listings(request):
     """
     A view to show all listings and include filtered by category
     """
-    listings = Listing.objects.all()
+    listings = Listing.objects.filter(is_active=True)
     filtered_listings = ListingFilter(request.GET, queryset=listings)
 
     return render(request, "listings.html", {'filter': filtered_listings})
