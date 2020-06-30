65d9aebeb31c2d564adeed5c719d814fe15321e3
Maripia Kuti <pionste@gmail.com>
2020-06-30 11:42:47 +0000

    Removed double aws configuration

--- corklagos/settings.py
+++ corklagos/settings.py
@@ -208,11 +208,6 @@ STATICFILES_DIRS = (
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-# Cache control
-AWS_S3_OBJECT_PARAMETERS = {
-    'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
-    'CacheControl': 'max-age=94608000'}
-
 # Storage configuration
 if 'USE_AWS' in os.environ:
     # Cache control
